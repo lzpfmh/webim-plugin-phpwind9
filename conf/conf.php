c494ea749e2eb8c58c695efef9b87aebb15c05cc
erylee <ery.lee@gmail.com>
2013-08-04 16:49:47 +0800

    fix conf

--- conf/conf.php
+++ conf/conf.php
@@ -1,17 +1,17 @@
 <?php
   return array(
 	'isopen' => '1',
-	'domain' => 'test',
-	'apikey' => 'test',
-	'host' => 'localhost',
-	'port' => '8000',
+	'domain' => '',
+	'apikey' => '',
+	'host' => 'webim20.cn',
+	'port' => '8888',
 	'local' => 'zh-CN',
 	'emot' => 'default',
 	'opacity' => '80',
 	'theme' => 'base',
 	'show_realname' => '0',
 	'disable_room' => '0',
-	'disable_chatlink' => '0',
+	'disable_chatlink' => '1',
 	'disable_menu' => '1',
 );
 ?>
